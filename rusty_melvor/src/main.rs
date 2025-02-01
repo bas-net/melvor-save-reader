26dddd6810ad83620cb714fe575cbb62410c4c87
Bas Netten <bas.netten.da@outlook.com>
2025-02-01 15:32:44 +0100

    First command line arg parsing

--- rusty_melvor/src/main.rs
+++ rusty_melvor/src/main.rs
@@ -1,3 +1,12 @@
 fn main() {
+    let skill = match std::env::args().nth(1) {
+        Some(skill) => skill,
+        None => {
+            println!("Please provide a skill to train");
+            return;
+        }
+    };
+
     println!("Hello, world!");
+    println!("Training skill: {}", skill);
 }
